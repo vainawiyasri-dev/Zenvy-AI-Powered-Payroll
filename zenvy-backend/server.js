d3f8d402a65519889248f319c920accc446e4b59
Vainawiyasri Sivakumar <vainawiyasrisivakumar.04@gmail.com>
2026-01-09 20:30:40 +0530

    Update server.js

--- zenvy-backend/server.js
+++ zenvy-backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({ origin: "https://zenvy-aipayroll.netlify.app/" }));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -18,3 +18,4 @@ app.use(require("./middlewares/errorMiddleware"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
